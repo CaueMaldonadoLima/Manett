77e494d42ee3b7df05fccdddf806699bea4674e6
CaueMaldonadoLima <caue.maldonado@ufms.br>
2024-08-28 13:53:19 -0400

    feat: add palette and font family to project structure

---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+export const palette = { 
+    'primaryBlue' : '#1E3A5F',
+    'secondaryBlue' : '#3AAFA9',
+    'accentColor' : '#F08A5D',
+    'background' : '#F4F4F9',
+    'darkGray' : '#2D2D2D',
+    'success' : '#28A745',
+    'error' : '#DC3545',
+    'warning' : '#FFC107',
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,5 @@
+import { palette } from "@/styles/colors";
+import { fontFamily } from "@/styles/fontFamly";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -13,6 +15,8 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: palette,
+      fontFamily: fontFamily,
     },
   },
   plugins: [],
