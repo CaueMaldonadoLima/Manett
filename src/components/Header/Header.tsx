eedc6e00a8864dbd53bf3dc05e3bf4f05684319e
CaueMaldonadoLima <caue.maldonado@ufms.br>
2024-09-10 16:16:56 -0400

    feat: add Header Component structure

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import { Text } from "../Text/Text"
+
+const Header = () => {
+    return (
+        <header className="w-full text-center shadow-md px-2 py-4">
+            <Text>Mannet</Text>
+        </header>
+    )
+}
+
+export { Header }
