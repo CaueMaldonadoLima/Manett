3e4fe68c0043dbdeab2a27a02b9e9fe2959eb4de
CaueMaldonadoLima <caue.maldonado@ufms.br>
2024-09-11 20:46:12 -0400

    feat: add logo

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,10 +1,17 @@
 import { Text } from '@/components'
+import Image from 'next/image'
 import React from 'react'
 
 export default function page() {
   return (
     <div>
-        <Text>Teste</Text>
+         <Image
+            src="/images/logo-manett.png"
+            alt="Logo Manett"
+            height={100}
+            width={100}>
+        </Image>
+        {/* <Text>Teste</Text> */}
     </div>
   )
 }
