ee1a03216ffecee1dbe377098ab81d4e4df7e5b2
lutzzdias <thiagolutzdias@hotmail.com>
2024-10-03 19:40:20 -0400

    feat: entries endpoints
    create, getById, getByUserId, update and delete

---
+++ src/app/api/entries/route.ts
@@ -0,0 +1,42 @@
+import { db } from "@/lib/db";
+import { generateIdFromEntropySize } from "lucia";
+import { entries } from "./schema";
+
+export async function POST(request: Request) {
+  const {
+    title,
+    description,
+    value,
+    type,
+    date,
+    userId,
+    categoryId,
+    bankAccountId,
+  } = await request.json();
+
+  // TODO: validate input
+
+  const id = generateIdFromEntropySize(10); // 16 characters long
+  const entry = {
+    id: id,
+    title: title,
+    description: description,
+    value: value,
+    type: type,
+    date: date,
+    userId: userId,
+    categoryId: categoryId,
+    bankAccountId: bankAccountId,
+  };
+  const [result] = await db.insert(entries).values(entry).returning();
+  console.log("CCCCC");
+
+  if (!result)
+    return Response.json({ status: 500, message: "Error creating entry" });
+
+  return Response.json({
+    status: 200,
+    message: "Entry created",
+    data: result,
+  });
+}
---
+++ src/app/api/entries/user/[id]/route.ts
@@ -0,0 +1,25 @@
+import { db } from "@/lib/db";
+import { eq } from "drizzle-orm";
+import { entries } from "../../schema";
+
+// get all user categories
+export async function GET(_: Request, { params }: { params: { id: string } }) {
+  const { id: userId } = params;
+
+  const result = await db
+    .select()
+    .from(entries)
+    .where(eq(entries.userId, userId));
+
+  if (!result)
+    return Response.json({
+      status: 404,
+      message: `No entries found for user with id: ${userId}`,
+    });
+
+  return Response.json({
+    status: 200,
+    message: "User entries retrieved successfully",
+    data: result,
+  });
+}
