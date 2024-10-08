12be294a386edafcce412804a0f9b57021aeabe4
CaueMaldonadoLima <caue.maldonado@ufms.br>
2024-10-08 15:56:24 -0400

    feat: add color schema and font family

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -4,14 +4,23 @@ import React from 'react'
 
 export default function page() {
 	return (
-		<div>
-			<Image
-				src="/images/logo-manett.png"
-				alt="Logo Manett"
-				height={100}
-				width={100}>
-			</Image>
-			<Text>Teste</Text>
-		</div>
+		<main className='bg-background bg-opacity-90 space-x-16 flex w-full h-screen px-64 py-16'>
+			<div className='bg-background w-full h-full flex rounded-lg shadow-lg items-center justify-center'>
+				<div className='w-1/2 items-center flex flex-col h-full justify-center text-center'>
+					<Text className='text-3xl font-bold'>Login</Text>
+					<input type="text" />
+					<input type="text" />
+					<input type="text" />
+				</div>
+			</div>
+			<div className='bg-background w-full h-full flex rounded-lg shadow-lg items-center justify-center'>
+				<div className='w-1/2 items-center flex flex-col h-full justify-center text-center'>
+					<Text className='text-3xl font-bold'>Login</Text>
+					<input type="text" />
+					<input type="text" />
+					<input type="text" />
+				</div>
+			</div>
+		</main>
 	)
 }
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 
 type TextProps = {
-    fontWeight?: 'thin' | 'extralight' | 'light' | 'normal' | 'medium' | 'semibold' | 'bold' | 'extrabold' | 'black'; 
-    fontSize?: number;
     className?: string;
     children: React.ReactNode;
 }
 
 const Text : React.FC<TextProps> = ({
-    fontWeight,
-    fontSize,
     className,
     children
 }) => {
     return (
         <span
             className={`
-                ${fontWeight ? `font-${fontWeight}` : ''}
-                ${fontSize ? `text-${fontSize}` : ''}
+                text-text
                 ${className}
         `}
         >
