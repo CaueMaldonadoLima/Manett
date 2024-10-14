b7a8375b206f73d385d4732af1c9096922fdde30
CaueMaldonadoLima <caue.maldonado@ufms.br>
2024-10-14 15:42:15 -0400

    feat: add sign in components

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,10 +1,11 @@
-import { InputText, Text } from '@/components'
 import { ArrowRightIcon } from '@/components/icons'
-import { LoginCard, LoginForm, LoginTitle } from '@/components/PageComponents/Login'
+import { LoginCard, LoginForm, LoginTitle, SigninForm } from '@/components/PageComponents/Login'
 import Image from 'next/image'
 import React from 'react'
 
 export default function page() {
+	const isLogin = true
+
 	return (
 		<main className='bg-background flex-1 bg-opacity-90 space-x-16 flex-col w-full px-24 h-screen'>
 			<div className='px-16 py-4 w-full items-center justify-start'>
@@ -16,27 +17,54 @@ export default function page() {
 				/>
 			</div>
 			<div className='flex gap-6'>
-				<div className='w-full flex	rounded-lg p-16 bg-accent'>
-					.
-				</div>
-				<LoginCard>
-					<LoginTitle title={'Get Started'} subtitle={'Create an account now!'}/>
-					<LoginForm />
-					<div>
-						<button className='w-full bg-primary p-2 rounded-md my-2'>
-							<div className='text-white flex w-full items-center justify-center'>
-								<span className='mr-2'>
-									Join us 
-								</span>
-								{ArrowRightIcon()}
+				{isLogin? (
+					<>
+						<LoginCard>
+							<LoginTitle title={'Welcome back'} subtitle='Continue with Google or enter your details.'/>
+							<SigninForm/>
+							<div>
+								<button className='w-full bg-primary p-2 rounded-md my-2'>
+									<div className='text-white flex w-full items-center justify-center'>
+										<span className='mr-2'>
+											Sign Up
+										</span>
+									</div>
+								</button>
+								<div className='w-full gap-2 flex flex-row'>
+									<span className='text-secondary'>Don't have an account yet?</span>
+									<button className='text-primary'>Sign up</button>  
+								</div>
 							</div>
-						</button>
-						<div className='w-full gap-2 flex flex-row'>
-							<span className='text-secondary'>Already have an account?</span>
-							<button className='text-primary'>Sign in</button>  {/* TODO: link to sign in page */}
+						</LoginCard>
+						<div className='w-full flex	rounded-lg p-16 bg-accent'>
+							.
 						</div>
-					</div>
-				</LoginCard>
+					</>
+				) : (
+					<>
+						<div className='w-full flex	rounded-lg p-16 bg-accent'>
+							.
+						</div>
+						<LoginCard>
+							<LoginTitle title={'Get Started'} subtitle={'Create an account now!'}/>
+							<LoginForm />
+							<div>
+								<button className='w-full bg-primary p-2 rounded-md my-2'>
+									<div className='text-white flex w-full items-center justify-center'>
+										<span className='mr-2'>
+											Join us 
+										</span>
+										{ArrowRightIcon()}
+									</div>
+								</button>
+								<div className='w-full gap-2 flex flex-row'>
+									<span className='text-secondary'>Already have an account?</span>
+									<button className='text-primary'>Sign in</button>  {/* TODO: link to sign in page */}
+								</div>
+							</div>
+						</LoginCard>
+					</>
+				)}
 			</div>
 		</main>
 	)
