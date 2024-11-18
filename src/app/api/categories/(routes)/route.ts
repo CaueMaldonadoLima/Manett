f342de772313d5524c0699d93e2df5d2dd86ce3e
lutzzdias <thiagolutzdias@hotmail.com>
2024-11-18 17:15:23 -0400

    refactor: use layers within category route handlers

---
+++ src/app/api/categories/(routes)/route.ts
@@ -0,0 +1,15 @@
+import controller from "@/app/api/entries/controller";
+import util from "@/app/api/utils";
+
+export async function POST(request: Request) {
+  const sessionId = util.getSessionId();
+
+  try {
+    const data = await request.json();
+
+    const result = await controller.create(data, sessionId);
+    return util.successResponse(result, "Category created successfully");
+  } catch (error) {
+    return util.errorResponse(error as Error);
+  }
+}
