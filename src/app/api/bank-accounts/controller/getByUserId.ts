53d67fde255d30b9d4872022347720ee5f19958c
lutzzdias <thiagolutzdias@hotmail.com>
2024-12-18 19:31:36 -0300

    feat: bank account controllers

---
+++ src/app/api/bank-accounts/controller/getByUserId.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+import useCase from "@/app/api/bank-accounts/useCase";
+import util from "@/app/api/utils";
+import { UnauthenticatedError } from "@/app/api/errors";
+
+const schema = z
+  .object({
+    userId: z.string().trim().min(1),
+  })
+  .strict();
+
+export async function getByUserId(
+  input: z.infer<typeof schema>,
+  sessionId: string | undefined,
+) {
+  if (!sessionId)
+    throw new UnauthenticatedError(
+      "Must be logged in to get bank account info",
+    );
+
+  const data = util.validateInput(input, schema);
+
+  return await useCase.getByUserId(data.userId, sessionId);
+}
