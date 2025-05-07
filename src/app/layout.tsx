d3d61c0fa7413db7bac06aec17b3d0585e4e7e6e
CaueMaldonadoLima <caue.maldonado@ufms.br>
2025-05-07 13:46:30 -0400

    feat: add card and button components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
   	children: React.ReactNode;
 }>) {
   	return (
-    	<html lang="en">
-      	<body className={`${poppins.className}`}>{children}</body>
+    	<html lang="en" className="h-full">
+      		<body className={`${poppins.className} h-full`}>{children}</body>
     	</html>
   	);
 }
