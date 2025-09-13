c54134ec21f96574ba3c8a13ff20115637ebee55
CaueMaldonadoLima <caue.maldonado@ufms.br>
2025-09-13 02:52:18 -0400

    style: add primary-foreground variable to tailwind.config

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
-          "border border-input bg-background shadow-sm hover:bg-primary/20 hover:text-primary-foreground",
+          "border border-input bg-background shadow-sm hover:bg-primary/20 hover:text-primary",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
